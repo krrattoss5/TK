164e9f6b98c6849b0fa66443b772862670b4e2fd
krrattoss5 <krrattoss5@gmail.com>
2023-09-25 19:05:38 -0500

    radiobuttons list

--- radio.py
+++ radio.py
@@ -6,9 +6,22 @@ root.title('Radio')
 r = IntVar()
 r.set('2')
 
-Radiobutton(root,text='option 1', variable=r, value=1).pack()
-Radiobutton(root,text='option 2', variable=r, value=2).pack()
+CHANCHITOS = [
+  ('Feliz', 'Feliz'),
+  ('Triste', 'Triste'),
+  ('Amargado', 'Amargado'),
+  ('Wolfgang', 'Wolfgang')
+]
 
-Label(root, textvariable=r).pack()
+chanchito = StringVar()
+chanchito.set('Wolfgang')
+
+for text, chancho in CHANCHITOS:
+  Radiobutton(root,text=text,variable=chanchito,value=chancho).pack()
+
+#Radiobutton(root,text='option 1', variable=r, value=1).pack()
+#Radiobutton(root,text='option 2', variable=r, value=2).pack()
+
+Label(root, textvariable=chanchito).pack()
 
 root.mainloop()
