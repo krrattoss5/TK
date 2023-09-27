d8bf4a44aeb95cd7b9225ff259b4c9a4e9fdb979
krrattoss5 <krrattoss5@gmail.com>
2023-09-26 22:01:02 -0500

    file

---
+++ file.py
@@ -0,0 +1,17 @@
+from tkinter import *
+from PIL import ImageTk,Image
+from tkinter import filedialog
+
+root = Tk()
+root.title('Archivos')
+
+root.filename = filedialog.askopenfilename(title='Elige una imagen',filetypes=(('Archivos PNG','*.png'),('Todos','*')))
+
+l = Label(root,text=root.filename).pack()
+
+img = Image.open(root.filename)
+ImgTk = ImageTk.PhotoImage(img)
+
+l2 = Label(root,image=ImgTk).pack()
+
+root.mainloop()
