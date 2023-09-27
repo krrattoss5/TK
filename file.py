8ed6fc93772e09d9c59d2b6497880b2983e17ded
krrattoss5 <krrattoss5@gmail.com>
2023-09-27 17:26:05 -0500

    files fixed

--- file.py
+++ file.py
@@ -5,13 +5,26 @@ from tkinter import filedialog
 root = Tk()
 root.title('Archivos')
 
-root.filename = filedialog.askopenfilename(title='Elige una imagen',filetypes=(('Archivos PNG','*.png'),('Todos','*')))
+# root.filename = filedialog.askopenfilename(title='Elige una imagen',filetypes=(('Archivos PNG','*.png'),('Todos','*')))
 
-l = Label(root,text=root.filename).pack()
+# l = Label(root,text=root.filename).pack()
 
-img = Image.open(root.filename)
-ImgTk = ImageTk.PhotoImage(img)
+# img = Image.open(root.filename)
+# ImgTk = ImageTk.PhotoImage(img)
 
-l2 = Label(root,image=ImgTk).pack()
+# l2 = Label(root,image=ImgTk).pack()
+
+def open():
+  global imgTk
+  root.filename = filedialog.askopenfilename(title='Elige una imagen',filetypes=(('Archivos PNG','*.png'),('Todos','*')))
+
+  l = Label(root,text=root.filename).pack()
+
+  img = Image.open(root.filename)
+  imgTk = ImageTk.PhotoImage(img)
+
+  l2 = Label(root,image=imgTk).pack()
+
+btn = Button(root,text='Cargar Imagen',command=open).pack()
 
 root.mainloop()
